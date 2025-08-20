9802075406304606f4ef7d0cdb1a4d083f72f545
Greg Saab <greg@timescale.com>
2025-08-20 08:48:50 -0400

    no need for no stinkin' mutex

--- src/util/store.ts
+++ src/util/store.ts
@@ -1,4 +1,3 @@
-import { Mutex, MutexInterface } from 'async-mutex';
 interface StoreProps<T> {
   contents?: T;
   fetch: () => Promise<T>;
@@ -6,27 +5,16 @@ interface StoreProps<T> {
 }
 
 export class Store<T> {
-  private mutex: MutexInterface;
-  private contents: T | null;
+  private contents: Promise<T> | null;
   private fetch: StoreProps<T>['fetch'];
 
-  constructor({ contents, fetch, fetchOnInit }: StoreProps<T>) {
-    this.contents = contents ?? null;
+  constructor({ contents, fetch }: StoreProps<T>) {
+    this.contents = contents ? Promise.resolve(contents) : null;
     this.fetch = fetch;
-    this.mutex = new Mutex();
-
-    if (fetchOnInit) {
-      this.get();
-    }
   }
 
   async get(): Promise<T> {
-    const release = await this.mutex.acquire();
-    if (!this.contents) {
-      this.contents = await this.fetch();
-    }
-
-    release();
+    this.contents ??= this.fetch();
 
     return this.contents;
   }
